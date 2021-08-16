ff4df778f87cc5dacb4afef10733d47530bb832e
Clint Riley <clint.riley@sportshubtech.com>
2021-08-16 12:37:49 -0500

    Geolocation example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react"
 import './App.css';
 
-function App() {
+
+const getUserCoords = (success, failure) => {
+  console.log("Entering contest")
+  navigator.geolocation.getCurrentPosition(success, failure)
+}
+
+const enterContestWithGeo = (setCurrentData, setGettingLocation) => {
+  const success = (successPosition) => {
+    console.log(`Current position : ${successPosition}`)
+    console.log(`Lat: ${successPosition.coords.latitude}`)
+    console.log(`Long: ${successPosition.coords.longitude}`)
+    fetch(`https://httpbin.org/anything?lat=${successPosition.coords.latitude}&long=${successPosition.coords.longitude}`, )
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          setCurrentData(`lat: ${data.args.lat} - lon: ${data.args.long}`)
+            setGettingLocation(false)
+        })
+  }
+  const failure = (failureReason) => {
+    fetch('https://httpbin.org/anything?lat=0&long=0', )
+          .then(response => response.json())
+          .then(data => {
+            console.log(data)
+            setCurrentData(`User denied location ${failureReason}`)
+            setGettingLocation(false)
+          })
+  }
+
+  setGettingLocation(true)
+  getUserCoords(success, failure)
+}
+
+
+const EnterContestButton = ({ setCurrentData }) => {
+  const [gettingLocation, setGettingLocation] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={() => enterContestWithGeo(setCurrentData, setGettingLocation)} disabled={gettingLocation}>
+      {
+        gettingLocation ? "Getting location..." : "Enter Contest"
+      }
+    </button>
+  )
+}
+
+const App = () => {
+  const [currentData, setCurrentData] = useState("No data yet")
+  return ( <div>
+    <h3> Geolocation example </h3>
+    <EnterContestButton setCurrentData={setCurrentData}/>
+    <div>
+      {currentData}
+    </div>
     </div>
   );
 }
+
 
 export default App;
